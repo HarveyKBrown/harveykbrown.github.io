df332043e2e716c56fd1c4324f346e17d626864e
Harvey Brown <validifyed@gmail.com>
2026-06-30 15:02:59 +0100

    improved formatting and personality

--- src/pages/og-image/_ogMarkup.ts
+++ src/pages/og-image/_ogMarkup.ts
@@ -6,9 +6,9 @@ export const ogMarkup = (title: string, pubDate: string) =>
 	html`<div tw="flex flex-col w-full h-full bg-[#1d1f21] text-[#c9cacc]">
 		<div tw="flex flex-col flex-1 w-full p-10 justify-center">
 			<p tw="text-2xl mb-6">${pubDate}</p>
-			<h1 tw="text-6xl font-bold leading-snug text-white">${title}</h1>
+			<h1 tw="text-6xl font-bold leading-snug text-[#C7C7D2]">${title}</h1>
 		</div>
-		<div tw="flex items-center justify-between w-full p-10 border-t-2 border-[#2bbc89] text-white">
+		<div tw="flex items-center justify-between w-full p-10 border-t-2 border-[#2bbc89] text-[#C7C7D2]">
 			<p tw="text-2xl ml-3 font-semibold">${siteConfig.title}</p>
 			<p>by ${siteConfig.author}</p>
 		</div>
